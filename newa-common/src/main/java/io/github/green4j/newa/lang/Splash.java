90c31c1829b9d4dd445b275eed59f9eaf9dfbe81
Anatolii Gudkov <agudkov@deltixlab.com>
2024-12-02 16:57:25 +0100

    ThreadLocal StringBuilder must be cleaned up berore reusing

--- newa-common/src/main/java/io/github/green4j/newa/lang/Splash.java
+++ newa-common/src/main/java/io/github/green4j/newa/lang/Splash.java
@@ -1,2 +1,9 @@
-package io.github.green4j.newa.lang;public class Splash {
+package io.github.green4j.newa.lang;
+
+public class Splash {
+    _______  ________  ________  ________
+  ╱╱   ╱   ╲╱        ╲╱  ╱  ╱  ╲╱        ╲
+ ╱╱        ╱         ╱         ╱         ╱
+╱         ╱        _╱         ╱         ╱
+╲__╱_____╱╲________╱╲________╱╲___╱____╱
 }
